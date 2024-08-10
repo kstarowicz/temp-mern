9c1b84dac4425e4de6cb97256f4162675d733963
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-10 18:49:41 +0200

    serch proble

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Buffer } from 'buffer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,11 +8,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5100/api',
+        target: 'http://localhost:5100',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+      define: {
+    'global.Buffer': Buffer,
+      }
   },
 });
 
@@ -29,7 +33,7 @@ export default defineConfig({
 //   server: {
 //     proxy: {
 //       '/api': {
-//         target: 'http://localhost:5100',
+//         target: 'http://localhost:5100/api',
 //         changeOrigin: true,
 //         rewrite: (path) => path.replace(/^\/api/, ''),
 //       },
